87f4eb7129d9cd636c8a236a5580f8ad652d4a97
Rajan kumar <git config --global user.email>
2023-12-05 04:50:08 +0530

    The Webapp is ready

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 app=Flask(__name__)
 ## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
-# scalar=pickle.load(open('scaling.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -23,22 +23,15 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
 if __name__=="__main__":
     app.run(debug=True)
-
-
-
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     data=[float(x) for x in request.form.values()]
-#     final_input=scalar.transform(np.array(data).reshape(1,-1))
-#     print(final_input)
-#     output=regmodel.predict(final_input)[0]
-#     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
-
-
-
-# if __name__=="__main__":
-#     app.run(debug=True)
    
      
